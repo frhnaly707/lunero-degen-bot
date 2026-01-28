2cc28382c07805141905c1e1077a7d2484a38d88
Farhan Aly <farhanaly707@gmail.com>
2026-01-28 23:03:47 +0800

    Reformat config.py for consistency and clarity

--- config.py
+++ config.py
@@ -5,13 +5,11 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", 0))
-MORALIS_API_KEY = os.getenv("MORALIS_API_KEY")
+MORALIS_API_KEY = os.getenv("MORALIS_API_KEY")  # ← WAJIB
 
-# Topic IDs
 SIGNAL_TOPIC_ID = int(os.getenv("SIGNAL_TOPIC_ID", 0))
 ANALYZE_TOPIC_ID = int(os.getenv("ANALYZE_TOPIC_ID", 0))
 TRADE_TOPIC_ID = int(os.getenv("TRADE_TOPIC_ID", 0))
 PORTFOLIO_TOPIC_ID = int(os.getenv("PORTFOLIO_TOPIC_ID", 0))
 AUTOPSY_TOPIC_ID = int(os.getenv("AUTOPSY_TOPIC_ID", 0))
 GAS_TOPIC_ID = int(os.getenv("GAS_TOPIC_ID", 0))
-
