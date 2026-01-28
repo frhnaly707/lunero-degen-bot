55f543b78f1d6e4fec013e41201692a9045b7708
Farhan Aly <farhanaly707@gmail.com>
2026-01-28 21:52:26 +0800

    Add BITQUERY_API_KEY to config
    Added BITQUERY_API_KEY to environment variables.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
@@ -5,11 +6,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", 0))
+BITQUERY_API_KEY = os.getenv("BITQUERY_API_KEY")  # ← Ini harus ada
 
-# Topic IDs
 SIGNAL_TOPIC_ID = int(os.getenv("SIGNAL_TOPIC_ID", 0))
 ANALYZE_TOPIC_ID = int(os.getenv("ANALYZE_TOPIC_ID", 0))
-TRADE_TOPIC_ID = int(os.getenv("TRADE_TOPIC_ID", 0))
-PORTFOLIO_TOPIC_ID = int(os.getenv("PORTFOLIO_TOPIC_ID", 0))
-AUTOPSY_TOPIC_ID = int(os.getenv("AUTOPSY_TOPIC_ID", 0))
-GAS_TOPIC_ID = int(os.getenv("GAS_TOPIC_ID", 0))
+# ... (topic lainnya)
